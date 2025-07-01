168b79fd824868821c26479172cc4996a3efd7b8
KVDS3 <153453864+KVDS3@users.noreply.github.com>
2025-06-30 18:58:28 -0600

    Add files via upload

---
+++ BodegaUrbana/backend/db_pruebas.sql
@@ -0,0 +1,8 @@
+Create database BodegaUrbana;
+
+use BodegaUrbana;
+
+create table usuarios(
+    id int auto_increment primary key,
+    nombre varchar(50) not null
+);
---
+++ BodegaUrbana/frontend/src/app/components/gestion-de-productos/gestion-de-productos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gestion-de-productos',
+  imports: [],
+  templateUrl: './gestion-de-productos.component.html',
+  styleUrl: './gestion-de-productos.component.css'
+})
+export class GestionDeProductosComponent {
+
+}
